b97394f798c4e9efcacc9150214dd369fd07787d
lunatics975 <116869422+lunatics975@users.noreply.github.com>
2024-02-09 00:25:39 +0000

    add comments to app.js

--- app.js
+++ app.js
@@ -1,56 +1,52 @@
-/*const express = require('express')
-const app = express()
-//socket.io setup
-const http = require('http');
-const server = http.createServer(app);
-const io = new Server(server);
-const { Server } = require('socket.io');
-const port = 3000
 
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
-server.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})*/
-
+//
+//This is the main backend file
+//
+//DONT TOUCH UNLESS YOU KNOW WHAT YOU ARE DOING -BEGIN
+//Creates a server with socket.io which is from a server which is
+////again from another with express <-?
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
-
+//Setting server up
 const app = express();
+//Creates a server based on app
 const server = createServer(app);
 const io = new Server(server);
+//The files in the folder public will be seen in the browser (frontend)
+//And all files can have access to the files in the public folder
 app.use(express.static('public'))
+//App gets the index.html file which is teh main file
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname + '/public/index.html'));
 });
+//--END
 
+//Creates list of players with the objects
 const players = {
 
 }
-
+//if is on connection console.log: a user is conected
+//socket id (random string) generated
 io.on('connection', (socket) => {
   console.log('a user connected');
   players[socket.id] = {
     x: 10
+    //fill in more information e.g. ELO, chess rating...
 
   }
+  //broadcast to other devices event: updtplayer,
+  //the objlist players is broadcasted
   io.emit('updtplayer', players)
+  //outputs every players object and thier attributes
   console.log(players);
+  //At disconnect, outputs user disconnected
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
-
+//server listens on port 3000
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
