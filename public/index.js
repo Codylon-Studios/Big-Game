7417c2053b2055e51774ba0e7f36e4ac9531fb81
baum456 <mingqi_li@icloud.com>
2024-02-09 13:24:01 +0100

    cahnge?

--- public/index.js
+++ public/index.js
@@ -1,5 +1,12 @@
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+canvas.width = 600
+canvas.height = 700
 const socket = io();
-ctx.fillStyle = 'red';
-ctx.fillRect(50, 50, 100, 100);
+c.fillStyle = "#ff9966"
+c.fillRect(0, 0, 200, 300)
+console.log("I'm working");
+
+socket.on('updtplayer', (players) =>{
+    
+})
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 Big Game
+Don't change the files in node_modules, json files
+all files in public are frontend
+app.js is backend 
+Still in Setting Up progress... Dont touch
