d07fea18e953bf71f936f6a416412a6f6bc1596d
lunatics975 <116869422+lunatics975@users.noreply.github.com>
2024-02-10 23:34:19 +0000

    add different html and js files for subsites, updt readme

---
+++ public/dashboard.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('myCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 400;
+ctx.fillRect(100,100,10,10);
+ctx.fillStyle="#42f5d1"
--- public/index.js
+++ public/index.js
@@ -1,10 +1,3 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-canvas.width = 600
-canvas.height = 700
-const socket = io();
-c.fillStyle = "#ff9966"
-c.fillRect(0, 0, 200, 300)
 console.log("I'm working");
 
 socket.on('updtplayer', (accounts) =>{
