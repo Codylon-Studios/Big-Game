20f133c5c96c5fc3f26786a953d6dbe5924bd3d8
Fabian Leonardi <fabian@bigigloo.de>
2024-02-11 20:33:58 +0100

    Added v2 of login/register

--- public/index.js
+++ public/index.js
@@ -12,9 +12,31 @@ socket.on('updtplayer', (accounts) =>{
 })
 
 document.getElementById("user").addEventListener("click", () => {
+    document.getElementById("account-select").style.visibility = "visible";
+});
+
+window.addEventListener("click", (ev) => {
+    if (ev.target != document.getElementById("user")) { // hides the dropdown if the user doesn't click on the icon
+        document.getElementById("account-select").style.visibility = "hidden";
+    }
+});
+
+document.getElementById("account-select-login").addEventListener("click", () => {
+    document.querySelector("#account-header > span").innerHTML = "Login";
+    document.getElementById("login-form").style.display = "block";
+    document.getElementById("register-form").style.display = "none";
+    document.getElementById("account-bg").style.visibility = "visible";
+});
+
+document.getElementById("account-select-register").addEventListener("click", () => {
+    document.querySelector("#account-header > span").innerHTML = "Register";
+    document.getElementById("register-form").style.display = "block";
+    document.getElementById("login-form").style.display = "none";
     document.getElementById("account-bg").style.visibility = "visible";
 });
 
 document.getElementById("close").addEventListener("click", () => {
     document.getElementById("account-bg").style.visibility = "hidden";
+    document.getElementById("login-form").style.display = "hidden";
+    document.getElementById("register-form").style.display = "hidden";
 });
