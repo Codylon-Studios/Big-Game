371c5dcb70455f9b417bd9fae05682cf70f83a59
lunatics975 <116869422+lunatics975@users.noreply.github.com>
2024-02-11 23:40:24 +0000

    cleaned up backend.js

--- backend.js
+++ backend.js
@@ -3,6 +3,7 @@
 //This is the main backend file
 //
 //DON'T TOUCH UNLESS YOU KNOW WHAT YOU ARE DOING
+//
 // Import necessary modules: express, http server, socket.io, pg and bcrypt
 const express = require('express');
 const { createServer } = require('node:http');
@@ -11,42 +12,36 @@ const { Server } = require('socket.io');
 const { Pool } = require('pg');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-
 // Initialize Express application
 const app = express();
-
 // Define saltRounds for bcrypt hashing
 const saltRounds = 10;
-
 // Create an HTTP server using Express
 const server = createServer(app);
-
 // Initialize Socket.io for real-time communication
 const io = new Server(server);
-
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// Serve index.html file when root URL is accessed
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname + '/public/index.html'));
+//Create a PostgreSQL connection pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'accounts',
+  password: 'postgres',
+  port: 5432,
 });
-
-// Store user accounts and their session IDs
+// Store session IDs
 const accounts = {};
 
+//
+//CONNECTION
+//
 // Handle socket connection event
 io.on('connection', (socket) => {
   console.log('a user connected');
-
   // Add the connected user to the accounts object
   accounts[socket.id] = {};
-
   // Emit 'updtplayer' event to update clients with current player information
   io.emit('updtplayer', accounts);
-
   console.log(accounts);
-
   // Handle socket disconnection event
   socket.on('disconnect', () => {
     console.log('a user disconnected');
@@ -61,19 +56,27 @@ server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
 
-// Create a PostgreSQL connection pool
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'accounts',
-    password: 'postgres',
-    port: 5432,
-});
-
+//
+//APP.USE
+//
 // Set up body-parser middleware to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-// Handle POST request to root route (for deleting user)
+//
+//APP.GET
+//
+//Serve index.html when root URL is accessed
+app.get('/',(req,res)=>{
+  res.sendFile(join(__dirname + '/public/index.html'))
+});
+
+//
+//APP.POST
+//
+
+// Handle POST request to root route (for deleting user)(original)
 app.post('/', async (req, res) => {
   /*const { deleteUsername, deletePassword } = req.body;
 
